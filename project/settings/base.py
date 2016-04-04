1714d2b7dc3610c9cec1fecce44eac8508800900
Gui Talarico <gtalarico@gmail.com>
2016-04-03 23:37:41 -0400

    Fix Static

--- project/settings/base.py
+++ project/settings/base.py
@@ -18,7 +18,8 @@ import os
 # SECRET_KEY=
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
+                          os.path.abspath(__file__))))
 
 
 ADMIN = (('Gui Talarico', 'gtalarico@gmail.com'),)
